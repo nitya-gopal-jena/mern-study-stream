14aec43943c3a6e995b75a02ce177ffd220ce52d
Nityagopaljena98 <nityagjena@hotmail.com>
2025-09-20 17:37:14 +0530

    useMemo file was added

---
+++ useMemo/src/App.jsx
@@ -0,0 +1,11 @@
+import UseMemo from "./components/useMemo";
+
+const App = () => {
+  return (
+    <>
+      <UseMemo />
+    </>
+  );
+};
+
+export default App;
---
+++ useMemo/src/components/UseMemo.jsx
@@ -0,0 +1,30 @@
+import { useMemo, useState } from 'react';
+
+const UseMemo = () => {
+  const [count, setCount] = useState(0);
+  const [color, setColor] = useState('blue');
+
+  function setNumber() {
+    console.log('SetNumber function');
+    for (let i = 0; i <= 100000000; i++) {}
+
+    return count * 10;
+  }
+
+  let getNumber = useMemo(setNumber, [count]);
+
+  return (
+    <>
+      {console.log('render')}
+      <h1>useMemo() hook</h1>
+      <div style={{ height: '200px', width: '200px', background: color, boxShadow: '0 0 4px black' }}></div>
+      <button onClick={() => setColor(color == "blue" ? "red" : "blue")}>Change Color</button>
+
+      <h1>Count: {count}</h1>
+      <button onClick={() => setCount(count + 1)}>Change Count</button>
+      <h1>My Number: {getNumber}</h1>
+    </>
+  );
+};
+
+export default UseMemo;
