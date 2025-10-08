f6d467ca576c1e565abdc8b51be1cc2dabc96cf8
Nityagopaljena98 <nityagjena@hotmail.com>
2025-10-08 17:21:49 +0530

    add fs module folder

---
+++ Node/modules/fs/index.js
@@ -0,0 +1,18 @@
+
+const fs = require('fs');
+
+
+// Creating file using Synchronous
+
+console.log("start");
+try {
+    fs.writeFileSync('./demo.txt', 'Hello World!')
+    console.log("File Created Successfully");
+    
+} catch (error) {
+    console.log('File Not Created Something Went Wrong', error.message);
+    
+}
+
+
+
