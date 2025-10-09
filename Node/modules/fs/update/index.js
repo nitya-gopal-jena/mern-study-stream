1d4a5b28b4e75bd472c2c81ef07217a0e9c5b580
Nityagopaljena98 <nityagjena@hotmail.com>
2025-10-09 16:53:20 +0530

    add updated file data asyncnronously

--- Node/modules/fs/update/index.js
+++ Node/modules/fs/update/index.js
@@ -1,5 +1,6 @@
 
 
+const { log } = require('console');
 const fs = require('fs');
 
 // Updated file data Synchronously 
@@ -12,6 +13,22 @@ try {
     console.log('File does not updated', error.message);
     
 }
+
+console.log("End");
+
+
+// Updated data file Asynchronously
+
+console.log("Start");
+fs.writeFile('../demo.txt', 'This is new updated data', (error) => {
+    if (error) {
+        console.log('File data not updated');
+        
+    } else {
+        console.log('data updated');
+        
+    }
+})
 
 console.log("End");
 
