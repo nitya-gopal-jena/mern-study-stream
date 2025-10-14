800e60e093708e0ad303efe7e8da1dc3642e5d66
Nityagopaljena98 <nityagjena@hotmail.com>
2025-10-14 17:25:22 +0530

    updated login form logic

--- Node/modules/http/index3.js
+++ Node/modules/http/index3.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+const { error } = require('console');
 const server = http.createServer((req, res) => {
     // console.log("client request come");
     // res.end("Hello");
@@ -46,7 +47,7 @@ const server = http.createServer((req, res) => {
 
                 // Required all fields for craete the uuser 
                 if (!user.name || !user.email || !user.password || !user.age) {
-                    return res.end(JSON.stringify({message:'All fields are required to create user'}))
+                    return res.end(JSON.stringify({ message: 'All fields are required to create user' }))
                 }
 
                 console.log(user);
@@ -69,8 +70,28 @@ const server = http.createServer((req, res) => {
 
             case '/login': req.on('data', (userDetails) => {
                 let user = JSON.parse(userDetails.toString())
+                fs.readFile(path.join(__dirname, 'users.json'), 'utf-8', (error, file) => {
+
+                    if (error) {
+                       return res.end(JSON.stringify({message:'Server side error'}))
+                    } 
+
+                    usersArr = JSON.parse(file)
+                    const isUser = usersArr.find((u) => u.email === user.email);
+                    
+                    if (!isUser) {
+                        return res.end(JSON.stringify({ message: 'Invalid Email' }))
+                    }
+
+                    if (isUser.password !== user.password) {
+                        return res.end(JSON.stringify({ message: 'Invalid Password' }))
+                    }
+
+                    return res.end(JSON.stringify({ message: 'user login successfully' }));
+     
+                })
+
                 console.log(user);
-                return res.end(JSON.stringify({ message: 'user details recieved in backend and printed' }));
             })
                 break;
 
