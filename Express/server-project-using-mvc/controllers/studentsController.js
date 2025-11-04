2ef7a75391ec3f38f50f03fb94a0074f2e99f2aa
Nityagopaljena98 <nityagjena@hotmail.com>
2025-11-04 17:57:42 +0530

    add student login controller and route

--- Express/server-project-using-mvc/controllers/studentsController.js
+++ Express/server-project-using-mvc/controllers/studentsController.js
@@ -1,9 +1,11 @@
 const fs = require('fs');
 const path = require('path');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
+// Student signup
 const studentSignup = async (req, res) => {
-    
+
     try {
 
         const { name, email, password, age } = req.body;
@@ -15,7 +17,7 @@ const studentSignup = async (req, res) => {
         const studentArr = JSON.parse(fs.readFileSync(path.join(__dirname, '../students.json'), 'utf-8'));
         const isStudent = studentArr.find((s) => s.email == email);
         if (isStudent) {
-            return res.status(409).json({message:'student with this email already exist'})
+            return res.status(409).json({ message: 'student with this email already exist' })
         }
 
         const hashPassword = await bcrypt.hash(password, 10);
@@ -23,15 +25,54 @@ const studentSignup = async (req, res) => {
         studentArr.push({ ...req.body, password: hashPassword });
         fs.writeFileSync(path.join(__dirname, '../students.json'), JSON.stringify(studentArr));
 
-        
+
     } catch (error) {
         console.log('error = ', error);
-        
+
         return res.status(500).json({ message: 'Server side error', error });
     }
 }
 
 
+// Student login
+
+const studentLogin = async (req, res) => {
+    try {
+
+        const { email, password } = req.body;
+        if (req.body === undefined) {
+            return res.status(400).json({ message: 'Provide all values' })
+        }
+
+        if (!email || !password) {
+            return res.status(400).json({ message: 'Provide all input fields' })
+        }
+
+        const studentArr = JSON.parse(fs.readFileSync(path.join(__dirname, '../students.json'), 'utf-8'));
+        const isStudent = studentArr.find((s) => s.email == email);
+        if (!isStudent) {
+            return res.status(400).json({ message: 'Invalid email' })
+        }
 
 
-module.exports = {studentSignup}
+        const isPassMatch = await bcrypt.compare(password, isStudent.password);
+        if (!isPassMatch) {
+            return res.status(400).json({ message: 'Invalid Password' });
+        }
+
+
+        const payload = { email: email, role: "student" };
+        const token = jwt.sign(payload, "student1234", { expiresIn: "1h" });
+
+        return res.status(200).json({ message: 'Login successfull', token });
+
+
+
+    } catch (error) {
+        return req.status(500).json({ message: 'Server side error', error })
+    }
+}
+
+
+
+module.exports = { studentSignup, studentLogin }
--- Express/server-project-using-mvc/routers/studentsRouter.js
+++ Express/server-project-using-mvc/routers/studentsRouter.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const { studentSignup } = require('../controllers/studentsController.js');
+const { studentSignup, studentLogin } = require('../controllers/studentsController.js');
 
 
 const studentsRouter = express.Router()
 
 
-studentsRouter.post('/signup', studentSignup)
-
+studentsRouter.post('/signup', studentSignup);
+studentsRouter.post('/login', studentLogin);
 
 module.exports = studentsRouter;
